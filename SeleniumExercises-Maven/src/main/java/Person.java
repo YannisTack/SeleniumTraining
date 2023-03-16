b03c58c10dfa472d18bb0fe806bf87b2919cc0e9
Yannis Tack <yannis.tack@hotmail.com>
2023-03-16 23:40:37 +0100

    Improved data holder Person

--- SeleniumExercises-Maven/src/main/java/Person.java
+++ SeleniumExercises-Maven/src/main/java/Person.java
@@ -6,30 +6,51 @@
  */
 public class Person {
 
+	private String firstName;
+	private String lastName;
+	private String sex;
+	private String email;
+	private String telephone;
+	private String company;
+	private String ssu;
+	private String seniority;
 	
-	private static final String NAME = "Tack";
-	private static final String FIRSTNAME = "Yannis";
-	private static final String SEX = "M";
-	private static final String EMAIL = "yannis.tack@ctg.com";
-	private static final String TELEPHONE = "0486273623";
-	private static final String COMPANY = "CTG";
-	private static final String SSU = "Testing";
-	private static final String SENIORITY = "Experienced";
-	
+	public Person(String firstName, String lastName, String sex, String email, String telephone, String company, String ssu, String seniority)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.sex = sex;
+		this.email = email;
+		this.telephone = telephone;
+		this.company = company;
+		this.ssu = ssu;
+		this.seniority = seniority;
+	}
+	public Person()
+	{
+		this.firstName = "Yannis";
+		this.lastName = "Tack";
+		this.sex = "M";
+		this.email = "yannis.tack@ctg.com";
+		this.telephone = "0486273623";
+		this.company = "CTG";
+		this.ssu = "Testing";
+		this.seniority = "Experienced";
+	}
 	
 	/**
 	 * Gets the name of this person.
 	 */
-	public static String getName() {return NAME;}
+	public String getName() {return lastName;}
 	/**
 	 * Gets the first name of this person.
 	 */
-	public static String getFirstName() {return FIRSTNAME;}
-	public static String getSex() {return SEX;}
-	public static String getEmail() {return EMAIL;}
-	public static String getTelephone() {return TELEPHONE;}
-	public static String getCompany() {return COMPANY;}
-	public static String getSSU() {return SSU;}
-	public static String getSeniority() {return SENIORITY;}
+	public String getFirstName() {return firstName;}
+	public String getSex() {return sex;}
+	public String getEmail() {return email;}
+	public String getTelephone() {return telephone;}
+	public String getCompany() {return company;}
+	public String getSSU() {return ssu;}
+	public String getSeniority() {return seniority;}
 
 }
